0f6135560c0e790c06ed27ada5ab55a35a7758e8
DayllonXavier <dayllonxavier@gmail.com>
2022-12-23 18:07:25 -0300

    Alterações nas portas

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
 CHAT_SERVER_HOST = "172.31.83.66"
-CHAT_SERVER_PORT = 5678
+CHAT_SERVER_PORT = 5685
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("172.31.94.219",6779), "Bob":("172.31.95.8",5680)}
+registry = {"Alice":("172.31.94.219",5686), "Bob":("172.31.95.8",5687)}
